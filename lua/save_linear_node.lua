181e9763375f607d4bbc7fbcd3e42ef118abecdb
Jonathan Tompson <jonathantompson@gmail.com>
2016-02-01 18:03:18 -0800

    improved linear node saving.

--- lua/save_linear_node.lua
+++ lua/save_linear_node.lua
@@ -6,15 +6,7 @@ function jtorch._saveLinearNode(node, ofile)
  
   assert(node.weight:dim() == 2, 'weight matrix is not 2D!')
   
-  -- This could be faster with vector notation, but is OK for now
-  for i=1,ninputs do
-    for v=1,noutputs do
-      -- weight is [nout, nin]  --> we want to store this column major
-      -- So each column is contiguous and is size nout
-      ofile:writeFloat(node.weight[{v, i}])  
-    end
-  end
-  -- jtorch._saveFloatTensorSafe(ofile, node.weight)  -- DOESN'T WORK! (even if I transpose it)
+  jtorch._saveFloatTensorSafe(ofile, node.weight:clone():t():contiguous())
   
   assert(node.bias:dim() == 1, 'bias vector is not 1D!')
   jtorch._saveFloatTensorSafe(ofile, node.bias)
